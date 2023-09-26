a20e3b2118a32b4f385e27d5cc4a4ce5a2a7441a
MarceloM47 <morinigomarcelo40@gmail.com>
2023-09-26 17:46:04 -0400

    Ajustes al navbar

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    require_once("C://wamp64/www/sistema_gym/view/head/head.php")
+    require_once("C://wamp64/www/sistema_gym_github/sistema_gym/view/head/head.php")
 ?>
 
-<!-- <a class="btn btn-primary" href="/sistema_gym/view/users/create.php">Agregar Usuario</a> -->
+<!-- <a class="btn btn-primary" href="/sistema_gym_github/sistema_gym/view/users/create.php">Agregar Usuario</a> -->
 
 <?php
-    require_once("C://wamp64/www/sistema_gym/view/head/footer.php")
+    require_once("C://wamp64/www/sistema_gym_github/sistema_gym/view/head/footer.php")
 ?>
--- view/head/head.php
+++ view/head/head.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GYM SUPREME</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="/sistema_gym/css/index.css">
+    <link rel="stylesheet" href="/sistema_gym_github/sistema_gym/css/index.css">
     
-    <script src="/sistema_gym/js/exportpdf.js"></script>
+    <script src="/sistema_gym_github/sistema_gym/js/exportpdf.js"></script>
     
     
 </head>
@@ -15,22 +15,19 @@
     
 <nav class="navbar navbar-expand-lg navbar-dark bg-black">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/sistema_gym">GYM</a>
+    <a class="navbar-brand" href="/sistema_gym_github/sistema_gym">GYM</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/sistema_gym">Pagina principal</a>
-        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Miembros
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/sistema_gym/view/users/create.php">Agregar miembro</a></li>
-            <li><a class="dropdown-item" href="/sistema_gym/view/users">Listado de miembros</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/users/create.php">Agregar miembro</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/users">Listado de miembros</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
@@ -38,8 +35,8 @@
             Empleados
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/sistema_gym/view/personas/create.php">Agregar empleado</a></li>
-            <li><a class="dropdown-item" href="/sistema_gym/view/personas">Listado de empleados</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/personas/create.php">Agregar empleado</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/personas">Listado de empleados</a></li>
           </ul>
         </li>
         </li>
@@ -48,8 +45,8 @@
             Roles
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/sistema_gym/view/users/create.php">Agregar rol</a></li>
-            <li><a class="dropdown-item" href="/sistema_gym/view/users">Listado de roles</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/users/create.php">Agregar rol</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/users">Listado de roles</a></li>
           </ul>
         </li>
         </li>
@@ -58,8 +55,8 @@
             Inventario
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/sistema_gym/view/inventario/create.php">Agregar inventario</a></li>
-            <li><a class="dropdown-item" href="/sistema_gym/view/inventario">Listado de inventario</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/inventario/create.php">Agregar inventario</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/inventario">Listado de inventario</a></li>
           </ul>
         </li>
         </li>
@@ -68,8 +65,8 @@
             Pagos
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/sistema_gym/view/users/create.php">Agregar pagos</a></li>
-            <li><a class="dropdown-item" href="/sistema_gym/view/users">Listado de pagos</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/users/create.php">Agregar pagos</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/users">Listado de pagos</a></li>
           </ul>
         </li>
         </li>
@@ -78,8 +75,8 @@
             Horarios
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/sistema_gym/view/users/create.php">Agregar horario</a></li>
-            <li><a class="dropdown-item" href="/sistema_gym/view/users">Listado de horario</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/users/create.php">Agregar horario</a></li>
+            <li><a class="dropdown-item" href="/sistema_gym_github/sistema_gym/view/users">Listado de horario</a></li>
           </ul>
         </li>
       </ul>
@@ -92,7 +89,7 @@
     <style>
         /* Agrega estilos para la imagen de fondo */
         body {
-            background-image: url('C:/wamp64/www/sistema_gym/img/gym.jpg'); /* Ruta de la imagen local */
+            background-image: url('C:/wamp64/www/sistema_gym_github/sistema_gym/img/gym.jpg'); /* Ruta de la imagen local */
             background-size: cover;
             background-attachment: fixed;
             background-position: center;
